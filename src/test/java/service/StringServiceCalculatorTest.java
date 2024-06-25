ace1b88828bed5703c39537df310bd714efaee3b
Senouci Moulay <mohamed31200@gmail.com>
2024-06-25 11:56:07 +0200

    first commit emptyStringTest

---
+++ src/test/java/service/StringServiceCalculatorTest.java
@@ -0,0 +1,23 @@
+package service;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = StringCalculatorService.class)
+public class StringServiceCalculatorTest {
+
+    @Autowired
+    private StringCalculatorService stringCalculatorService;
+
+    @Test
+    public void testResultOfMethodAddReturnZeroWhenStringIsEmpty() {
+        int result = stringCalculatorService.add("");
+        Assertions.assertEquals(0, result);
+    }
+}
