5cc5fa503e0f6700555ba68e186db3e501063c08
Senouci Moulay <mohamed31200@gmail.com>
2024-06-25 11:56:50 +0200

    implement method Add for emptyStringTest

--- src/main/java/service/StringCalculatorService.java
+++ src/main/java/service/StringCalculatorService.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StringCalculatorService {
-
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        throw new UnsupportedOperationException();
+    }
 }
 
